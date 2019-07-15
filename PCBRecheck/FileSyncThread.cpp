777dee16fda05a6cad338025421b3a08c0af9faf
Apollo4634 <apollo4634@gmail.com>
2019-07-15 11:21:06 +0800

    0715-1120 完善文件同步中的接收功能

--- PCBRecheck/FileSyncThread.cpp
+++ PCBRecheck/FileSyncThread.cpp
@@ -4,18 +4,24 @@ FileSyncThread::FileSyncThread(QObject *parent)
 	: QThread(parent)
 {
 	runtimeParams = Q_NULLPTR;
+
+	short t_port = 8888;
+	std::string t_workDirectory = "D:\\";
+	fileReceiver = new FileReceiver(t_port, t_workDirectory);
 }
 
 FileSyncThread::~FileSyncThread()
 {
 	qDebug() << "~FileSyncThread";
+	delete fileReceiver;
+	fileReceiver = Q_NULLPTR;
 }
 
 
 void FileSyncThread::run()
 {
 	//查询是否收到了新的检测结果
-	fileReceiver.startListen();
+	fileReceiver->startListen();
 
 	//如果有则发送到其他复查设备
 	// ...
